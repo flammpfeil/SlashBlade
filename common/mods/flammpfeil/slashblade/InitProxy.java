94e605fe93547a65aeca8ce5f6a0d52a95400bf7
flammpfeil <flammpfeil@gmail.com>
2013-12-12 02:07:58 +0900

    またもやpackage変更わすれ

--- common/mods/flammpfeil/slashblade/InitProxy.java
+++ common/mods/flammpfeil/slashblade/InitProxy.java
@@ -3,7 +3,7 @@ package mods.flammpfeil.slashblade;
 import cpw.mods.fml.common.SidedProxy;
 
 public class InitProxy {
-	@SidedProxy(clientSide = "mods.flammpfeil.sweapon.InitProxyClient", serverSide = "mods.flammpfeil.sweapon.InitProxy")
+	@SidedProxy(clientSide = "mods.flammpfeil.slashblade.InitProxyClient", serverSide = "mods.flammpfeil.slashblade.InitProxy")
 	public static InitProxy proxy;
 
 
