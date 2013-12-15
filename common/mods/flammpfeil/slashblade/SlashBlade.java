4d30f6a0b16a02f054c6415e795c2201d31b6e14
flammpfeil <flammpfeil@gmail.com>
2013-12-15 13:35:22 +0900

    エスケープできてなかった

--- common/mods/flammpfeil/slashblade/SlashBlade.java
+++ common/mods/flammpfeil/slashblade/SlashBlade.java
@@ -189,7 +189,7 @@ public class SlashBlade implements IFuelHandler ,ITickHandler{
 			for(String curEntry : propAttackableTargets.getStringList()){
 				int spliterIdx = curEntry.lastIndexOf(":");
 				String name = curEntry.substring(0, spliterIdx);
-				name = name.replace("%amp;","&");
+				name = name.replace(":amp:","&");
 				String attackableStr = curEntry.substring(spliterIdx + 1, curEntry.length());
 
 				boolean attackable = attackableStr.toLowerCase().equals("true");
@@ -201,7 +201,7 @@ public class SlashBlade implements IFuelHandler ,ITickHandler{
 				Boolean name = (Boolean)attackableTargets.get(key);
 
 				String keyStr = (String)key;
-				keyStr = keyStr.replace("&","%amp;");
+				keyStr = keyStr.replace("&",":amp:");
 				targets.add(String.format("%s:%b", keyStr ,name));
 			}
 
