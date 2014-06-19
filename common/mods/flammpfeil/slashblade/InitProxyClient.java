0aad4a36902b8a204995b1a4704babcb682e5bd3
flammpfeil <flammpfeil@gmail.com>
2014-06-20 05:06:36 +0900

    r31 キーコンフィグ赤化防止 blockpickキーコンフィグを流用するように(変更可能に） ※creativeでは、ピックアップが優先されるので使いづらいです

--- common/mods/flammpfeil/slashblade/InitProxyClient.java
+++ common/mods/flammpfeil/slashblade/InitProxyClient.java
@@ -6,9 +6,12 @@ import cpw.mods.fml.client.registry.KeyBindingRegistry;
 import cpw.mods.fml.client.registry.KeyBindingRegistry.KeyHandler;
 import cpw.mods.fml.client.registry.RenderingRegistry;
 import cpw.mods.fml.common.TickType;
+import cpw.mods.fml.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityClientPlayerMP;
 import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.client.MinecraftForgeClient;
 import net.minecraftforge.common.MinecraftForge;
 
@@ -28,43 +31,43 @@ public class InitProxyClient extends InitProxy{
 
         RenderingRegistry.registerEntityRenderingHandler(EntityDrive.class, new RenderDrive());
         RenderingRegistry.registerEntityRenderingHandler(EntityPhantomSword.class, new RenderPhantomSword());
-        KeyBinding[] array = new KeyBinding[]{};
-        array = (KeyBinding[])KeyBinding.keybindArray.toArray(array);
-        boolean[] repeat = new boolean[array.length];
-        for(int i=0;i<repeat.length;i++)
-        	repeat[i] = false;
+        KeyBinding[] array = new KeyBinding[]{Minecraft.getMinecraft().gameSettings.keyBindPickBlock};
+        boolean[] repeat = new boolean[]{false};
 
-        KeyBindingRegistry.registerKeyBinding(new KeyHandler(array,repeat) {
+        TickRegistry.registerTickHandler(new KeyHandler(array,repeat) {
 
-			@Override
-			public String getLabel() {
-				return null;
-			}
-
-			@Override
-			public EnumSet<TickType> ticks() {
-		        return EnumSet.of(TickType.CLIENT, TickType.RENDER);
-			}
-
-			@Override
-			public void keyUp(EnumSet<TickType> types, KeyBinding kb, boolean tickEnd) {
-				if(kb.keyCode == -98 && tickEnd){
-	                Minecraft mc = Minecraft.getMinecraft();
-	                EntityClientPlayerMP player = mc.thePlayer;
-	                mc.playerController.func_78768_b(player,player);
-	                /*
-	                if(player != null && mc.inGameHasFocus && mc.currentScreen == null){
-	                    player.sendChatMessage("/slashblade ps");
-	                }*/
+				@Override
+				public String getLabel() {
+					return null;
 				}
-			}
 
-			@Override
-			public void keyDown(EnumSet<TickType> types, KeyBinding kb,
-					boolean tickEnd, boolean isRepeat) {
-				// TODO 自動生成されたメソッド・スタブ
+				@Override
+				public EnumSet<TickType> ticks() {
+			        return EnumSet.of(TickType.CLIENT, TickType.RENDER);
+				}
 
-			}
-		});
+				@Override
+				public void keyUp(EnumSet<TickType> types, KeyBinding kb, boolean tickEnd) {
+					if(tickEnd){
+		                Minecraft mc = Minecraft.getMinecraft();
+		                EntityClientPlayerMP player = mc.thePlayer;
+		                if(player != null && mc.inGameHasFocus && mc.currentScreen == null){
+		                	ItemStack item = player.getHeldItem();
+		                	if(item != null){
+		                		if(item.getItem() instanceof ItemSlashBlade)
+		                			mc.playerController.func_78768_b(player,player);
+		                	}
+		                }
+					}
+				}
+
+				@Override
+				public void keyDown(EnumSet<TickType> types, KeyBinding kb,
+						boolean tickEnd, boolean isRepeat) {
+					// TODO 自動生成されたメソッド・スタブ
+
+				}
+			}, Side.CLIENT);
+        /*KeyBindingRegistry.registerKeyBinding();*/
 	}
 }
