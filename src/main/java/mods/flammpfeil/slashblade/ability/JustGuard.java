e799c45295467f6dfd5ff2b7ebe2920daecbf8cf
flammpfeil <flammpfeil@gmail.com>
2015-10-04 18:20:32 +0900

    ジャストガード成功時、状態異常も無効化する

--- src/main/java/mods/flammpfeil/slashblade/ability/JustGuard.java
+++ src/main/java/mods/flammpfeil/slashblade/ability/JustGuard.java
@@ -9,9 +9,14 @@ import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Created by Furia on 14/07/26.
@@ -84,6 +89,8 @@ public class JustGuard {
                 }
                 el.getEntityData().setDouble("SBLastPosY", el.posY + yOffset);
 
+                storePotionEffect(el);
+
                 ItemSlashBlade.IsCharged.set(tag,true);
                 ItemSlashBlade.OnClick.set(tag,true);
                 ItemSlashBlade.OnJumpAttacked.set(tag,false);
@@ -106,6 +113,10 @@ public class JustGuard {
             long cs = ChargeStart.get(el.getEntityData());
             cs = Math.max(interval,cs);
             if(cs < 0){
+
+                if(cs == -1)
+                    restorePotionEffect(el);
+
                 el.motionX = 0;
                 el.motionY = 0;
                 el.motionZ = 0;
@@ -115,6 +126,45 @@ public class JustGuard {
                 el.setPositionAndUpdate(el.posX,el.posY,el.posZ);
 
                 ChargeStart.set(el.getEntityData(),cs + 1);
+            }
+        }
+    }
+
+
+    void storePotionEffect(EntityLivingBase entity){
+        Collection effects = entity.getActivePotionEffects();
+        if (!effects.isEmpty())
+        {
+            NBTTagList nbttaglist = new NBTTagList();
+            Iterator iterator = effects.iterator();
+
+            while (iterator.hasNext())
+            {
+                PotionEffect potioneffect = (PotionEffect)iterator.next();
+                nbttaglist.appendTag(potioneffect.writeCustomPotionEffectToNBT(new NBTTagCompound()));
+            }
+
+            entity.getEntityData().setTag("SB_JG_LastEffects", nbttaglist);
+        }
+    }
+
+    void restorePotionEffect(EntityLivingBase entity){
+
+        if(!entity.getEntityData().hasKey("SB_JG_LastEffects")) return;
+
+        entity.clearActivePotions();
+
+        NBTTagList nbttaglist = entity.getEntityData().getTagList("SB_JG_LastEffects", 10);
+        entity.getEntityData().removeTag("SB_JG_LastEffects");
+
+        for (int i = 0; i < nbttaglist.tagCount(); ++i)
+        {
+            NBTTagCompound nbttagcompound1 = nbttaglist.getCompoundTagAt(i);
+            PotionEffect potioneffect = PotionEffect.readCustomPotionEffectFromNBT(nbttagcompound1);
+
+            if (potioneffect != null)
+            {
+                entity.addPotionEffect(potioneffect);
             }
         }
     }
