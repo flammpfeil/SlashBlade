fe16bdd48b04edaab0e83acfba363a5c8fd43b4c
flammpfeil <flammpfeil@gmail.com>
2015-12-20 02:07:24 +0900

    r71 無敵時間の処理を見直し ポーション＆炎上の無敵時間中の無視を追加

--- src/main/java/mods/flammpfeil/slashblade/ability/JustGuard.java
+++ src/main/java/mods/flammpfeil/slashblade/ability/JustGuard.java
@@ -1,10 +1,12 @@
 package mods.flammpfeil.slashblade.ability;
 
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.ReflectionHelper;
 import mods.flammpfeil.slashblade.ItemSlashBlade;
 import mods.flammpfeil.slashblade.TagPropertyAccessor;
 import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -89,8 +91,6 @@ public class JustGuard {
                 }
                 el.getEntityData().setDouble("SBLastPosY", el.posY + yOffset);
 
-                storePotionEffect(el);
-
                 ItemSlashBlade.IsCharged.set(tag,true);
                 ItemSlashBlade.OnClick.set(tag,true);
                 ItemSlashBlade.OnJumpAttacked.set(tag,false);
@@ -113,10 +113,6 @@ public class JustGuard {
             long cs = ChargeStart.get(el.getEntityData());
             cs = Math.max(interval,cs);
             if(cs < 0){
-
-                if(cs == -1)
-                    restorePotionEffect(el);
-
                 el.motionX = 0;
                 el.motionY = 0;
                 el.motionZ = 0;
@@ -126,45 +122,6 @@ public class JustGuard {
                 el.setPositionAndUpdate(el.posX,el.posY,el.posZ);
 
                 ChargeStart.set(el.getEntityData(),cs + 1);
-            }
-        }
-    }
-
-
-    void storePotionEffect(EntityLivingBase entity){
-        Collection effects = entity.getActivePotionEffects();
-        if (!effects.isEmpty())
-        {
-            NBTTagList nbttaglist = new NBTTagList();
-            Iterator iterator = effects.iterator();
-
-            while (iterator.hasNext())
-            {
-                PotionEffect potioneffect = (PotionEffect)iterator.next();
-                nbttaglist.appendTag(potioneffect.writeCustomPotionEffectToNBT(new NBTTagCompound()));
-            }
-
-            entity.getEntityData().setTag("SB_JG_LastEffects", nbttaglist);
-        }
-    }
-
-    void restorePotionEffect(EntityLivingBase entity){
-
-        if(!entity.getEntityData().hasKey("SB_JG_LastEffects")) return;
-
-        entity.clearActivePotions();
-
-        NBTTagList nbttaglist = entity.getEntityData().getTagList("SB_JG_LastEffects", 10);
-        entity.getEntityData().removeTag("SB_JG_LastEffects");
-
-        for (int i = 0; i < nbttaglist.tagCount(); ++i)
-        {
-            NBTTagCompound nbttagcompound1 = nbttaglist.getCompoundTagAt(i);
-            PotionEffect potioneffect = PotionEffect.readCustomPotionEffectFromNBT(nbttagcompound1);
-
-            if (potioneffect != null)
-            {
-                entity.addPotionEffect(potioneffect);
             }
         }
     }
--- src/main/java/mods/flammpfeil/slashblade/ability/UntouchableTime.java
+++ src/main/java/mods/flammpfeil/slashblade/ability/UntouchableTime.java
@@ -8,34 +8,47 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.potion.PotionEffect;
 import net.minecraftforge.event.entity.living.LivingAttackEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
+
+import java.util.Collection;
+import java.util.Iterator;
 
 /**
  * Created by Furia on 15/05/16.
  */
 public class UntouchableTime {
 
-    static final String UntouchableTimeOut = "SB.UntouchableTimeOut";
-    static final String IsAvoid = "SB.IsAvoid";
+    static private final String UntouchableTimeOut = "SB.UntouchableTimeOut";
+    static private final String IsAvoid = "SB.IsAvoid";
+    static private final String LastEffects = "SB.UT.LastEffects";
+    static private final String LastNoBurning = "SB.UT.LastNoBurning";
 
     static public void setUntouchableTime(Entity target, int ticks){
         setUntouchableTime(target,ticks,false);
     }
     static public void setUntouchableTime(Entity target, int ticks,boolean isAvoid){
-        target.getEntityData().setLong(UntouchableTimeOut,target.worldObj.getTotalWorldTime() + ticks);
+        target.getEntityData().setLong(UntouchableTimeOut, target.worldObj.getTotalWorldTime() + ticks);
 
         setIsAvoid(target, isAvoid);
+
+        if(target instanceof EntityLivingBase)
+            storePotionEffect((EntityLivingBase)target);
     }
 
-    static public void setIsAvoid(Entity target, boolean isAvoid){
-        target.getEntityData().setBoolean(IsAvoid,isAvoid);
+    static private void setIsAvoid(Entity target, boolean isAvoid){
+        target.getEntityData().setBoolean(IsAvoid, isAvoid);
+    }
+    static private boolean isAvoid(Entity target){
+        return target.getEntityData().getBoolean(IsAvoid);
     }
 
-    static public void removeUntouchableTag(Entity target){
+    static private void removeUntouchableTag(Entity target){
         NBTTagCompound tag = target.getEntityData();
-        String[] targetKeys = {UntouchableTimeOut,IsAvoid};
+        String[] targetKeys = {UntouchableTimeOut,IsAvoid,LastEffects,LastNoBurning};
 
         for(String key : targetKeys){
             if(tag.hasKey(key))
@@ -43,25 +56,45 @@ public class UntouchableTime {
         }
     }
 
+    static private boolean isUntouchable(EntityLivingBase target) {
+        if (target == null) return false;
+        if (target.worldObj == null) return false;
+        NBTTagCompound tag = target.getEntityData();
+        if (!tag.hasKey(UntouchableTimeOut)) return false;
+
+        long timeOut = tag.getLong(UntouchableTimeOut);
+        long now = target.worldObj.getTotalWorldTime();
+
+        if (timeOut < now) {
+            removeUntouchableTag(target);
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    static private void doAvoid(EntityLivingBase target){
+        if(!isAvoid(target)) return;
+
+        StylishRankManager.addRankPoint(target,StylishRankManager.AttackTypes.AttackAvoidance);
+        setIsAvoid(target, false);
+    }
+
+    private void WitchTime(Entity target, int duration) {
+        if (target == null) return;
+        if (!(target instanceof EntityLivingBase)) return;
+
+        StunManager.setStun((EntityLivingBase) target, duration);
+    }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
     public void onLivingHurt(LivingHurtEvent event){
         EntityLivingBase target = event.entityLiving;
-        if(target == null) return;
-        if(target.worldObj == null) return;
-        NBTTagCompound tag = target.getEntityData();
-        if(!tag.hasKey(UntouchableTimeOut)) return;
+        if(!isUntouchable(target)) return;
 
-        long timeOut = tag.getLong(UntouchableTimeOut);
+        doAvoid(target);
 
-        long now = target.worldObj.getTotalWorldTime();
-
-        if(timeOut < now) return;
-
-        if(tag.getBoolean(IsAvoid)){
-            StylishRankManager.addRankPoint(target,StylishRankManager.AttackTypes.AttackAvoidance);
-            setIsAvoid(target, false);
-        }
+        WitchTime(event.source.getEntity(), 10);
 
         event.setCanceled(true);
         event.ammount = 0;
@@ -70,39 +103,60 @@ public class UntouchableTime {
     @SubscribeEvent
     public void onLivingAttackEvent(LivingAttackEvent event){
         EntityLivingBase target = event.entityLiving;
-        if(target == null) return;
-        if(target.worldObj == null) return;
-        NBTTagCompound tag = target.getEntityData();
-        if(!tag.hasKey(UntouchableTimeOut)) return;
+        if(!isUntouchable(target)) return;
 
-        long timeOut = tag.getLong(UntouchableTimeOut);
+        doAvoid(target);
 
-        long now = target.worldObj.getTotalWorldTime();
-
-        if(timeOut < now) return;
-
-        if(tag.getBoolean(IsAvoid)){
-            StylishRankManager.addRankPoint(target,StylishRankManager.AttackTypes.AttackAvoidance);
-            setIsAvoid(target, false);
-        }
+        WitchTime(event.source.getEntity(), 10);
 
         event.setCanceled(true);
     }
 
     @SubscribeEvent
     public void LivingUpdateEvent(LivingEvent.LivingUpdateEvent event){
-        EntityLivingBase target = event.entityLiving;
-        if(target == null) return;
-        if(target.worldObj == null) return;
-        NBTTagCompound tag = target.getEntityData();
-        if(!tag.hasKey(UntouchableTimeOut)) return;
+        if(isUntouchable(event.entityLiving))
+            restorePotionEffect(event.entityLiving);
+    }
 
-        long timeOut = tag.getLong(UntouchableTimeOut);
+    static private void storePotionEffect(EntityLivingBase entity){
+        Collection effects = entity.getActivePotionEffects();
+        if (!effects.isEmpty())
+        {
+            NBTTagList nbttaglist = new NBTTagList();
+            Iterator iterator = effects.iterator();
 
-        long now = target.worldObj.getTotalWorldTime();
+            while (iterator.hasNext())
+            {
+                PotionEffect potioneffect = (PotionEffect)iterator.next();
+                nbttaglist.appendTag(potioneffect.writeCustomPotionEffectToNBT(new NBTTagCompound()));
+            }
 
+            entity.getEntityData().setTag(LastEffects, nbttaglist);
+        }
 
-        if(timeOut < now)
-            removeUntouchableTag(target);
+        if(!entity.isBurning())
+            entity.getEntityData().setBoolean(LastNoBurning,true);
+    }
+
+    static private void restorePotionEffect(EntityLivingBase entity){
+        if(entity.getEntityData().hasKey(LastNoBurning)){
+            ReflectionHelper.setPrivateValue(Entity.class, entity, 0, "fire", "field_70151_c");
+        }
+
+        if(!entity.getEntityData().hasKey(LastEffects)) return;
+
+        entity.clearActivePotions();
+
+        NBTTagList nbttaglist = entity.getEntityData().getTagList(LastEffects, 10);
+        for (int i = 0; i < nbttaglist.tagCount(); ++i)
+        {
+            NBTTagCompound tag = nbttaglist.getCompoundTagAt(i);
+            PotionEffect potioneffect = PotionEffect.readCustomPotionEffectFromNBT(tag);
+
+            if (potioneffect != null)
+            {
+                entity.addPotionEffect(potioneffect);
+            }
+        }
     }
 }
