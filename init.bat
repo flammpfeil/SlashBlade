8f74ee5532b627162fdf19273ad8c3b425918270
flammpfeil <flammpfeil@gmail.com>
2019-03-02 01:39:34 +0900

    整理

--- init.bat
+++
@@ -1,2 +0,0 @@
-call gradlew.bat setupDecompWorkspace -Dhttps.proxyHost=127.0.0.1 -Dhttp.proxyHost=127.0.0.1 -Dhttps.proxyPort=1080 -Dhttp.proxyPort=1080
-pause
--- setup.bat
+++
@@ -1,3 +0,0 @@
-//call gradlew.bat setupDevWorkspace
-call gradlew.bat idea
-pause
